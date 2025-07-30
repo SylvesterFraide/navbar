e6b8810b453ae551ab0107b71446e22cf9a1a7d2
SylvesterFraide <fridaysylvester2@gmail.com>
2025-07-30 19:33:53 +0300

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from './Component/NavBar';
+
+/*
 import { Navbar } from './Components/NavBar/Navbar';
 import { Home } from './Components/Home/Home';
 import { Product } from "./Components/Products/Product";
 import { Contact } from "./Components/Contact/Contact ";
 import { Services } from "./Components/Services/Services";
+*/
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NavBar />
+      { /* <Navbar /> */}
       <Routes>
+       { /*
         <Route path="/" element={<Home />} />
         <Route path="/Services" element={<Services />} />
         <Route path="/Product" element={<Product />} />
         <Route path="/Contact" element={<Contact />} />
+        */}
       </Routes>
     </BrowserRouter>
   );
